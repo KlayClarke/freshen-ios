3ea2d440fdd252eef76736e008eacfa42c0b568e
Klay Clarke <clarkeklay@gmail.com>
2022-06-04 07:18:08 -0400

    init

---
+++ freshen-ios/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  freshen-ios
+//
+//  Created by Klay Anthony Clarke on 6/3/22.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ freshen-ios/freshen_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  freshen_iosApp.swift
+//  freshen-ios
+//
+//  Created by Klay Anthony Clarke on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct freshen_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
