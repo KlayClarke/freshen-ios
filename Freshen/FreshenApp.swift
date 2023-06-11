76e7742ad47f811ce42f18860cfad5f899605956
Klay Clarke <clarkeklay@gmail.com>
2023-06-11 15:38:18 -0400

    Freshen start

---
+++ Freshen/FreshenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshenApp.swift
+//  Freshen
+//
+//  Created by Klay Anthony Clarke on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FreshenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
